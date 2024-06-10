78753219826c94f74d6ccc3be69faefca9407db1
nakitaki <nadya.mishevaa@gmail.com>
2024-06-10 21:34:21 +0300

    added race edit functionality

--- BlogTravel/Interfaces/IRaceRepository.cs
+++ BlogTravel/Interfaces/IRaceRepository.cs
@@ -6,8 +6,10 @@ namespace BlogTravel.Interfaces
     {
         Task<IEnumerable<Race>> GetAll();
         Task<Race> GetByIdAsync(int id);
-        Task<IEnumerable<Race>> GetAllRacesByCity(string city);
 
+        Task<Race> GetByIdAsyncNoTracking(int id);
+
+        Task<IEnumerable<Race>> GetAllRacesByCity(string city);
         bool Add(Race race);
         bool Update(Race race);
         bool Delete(Race race);
--- BlogTravel/Repository/RaceRepository.cs
+++ BlogTravel/Repository/RaceRepository.cs
@@ -40,6 +40,12 @@ namespace BlogTravel.Repository
             return await _context.Races.Include(i => i.Address).FirstOrDefaultAsync(i => i.Id == id);
         }
 
+        public async Task<Race> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+
         public bool Save()
         {
             var saved = _context.SaveChanges();
